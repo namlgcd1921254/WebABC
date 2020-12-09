b4f1964c32ac43cf82267ca82731465d6f9dc5fc
namlgcd1921254 <namle28102001@gmail.com>
2020-12-09 19:18:14 +0700

    Bo xung thong diep Hello word! vao file index.html va huong dan vao file index.php

---
+++ index.php
@@ -0,0 +1,4 @@
+
+[A[C[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D
+<?php header ( 'Location: /index.html')[D [C[ ) ; ? >
+[A[B<?php header ( 'Location: /index.hm[2tml ' ) ; ? >
